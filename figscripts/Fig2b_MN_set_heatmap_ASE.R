e32083926d56695da0bc14087190283406346be0
banwang27 <44854007+banwang27@users.noreply.github.com>
2023-03-01 11:13:29 -0800

    Create Fig2b_MN_set_heatmap_ASE.R

---
+++ figscripts/Fig2b_MN_set_heatmap_ASE.R
@@ -0,0 +1,49 @@
+library(ggplot2)  
+align2='human'
+TGF = c('NGF','BDNF','NTF3','NTF4','NTRK1','NTRK2','NTRK3','NGFR')
+TGF = data.frame('gene'=TGF)
+final=c()
+for (celltype in c('CM','MN','PP','HP','SKM','RPE')){
+    deseq2 = read.csv(paste0('/scratch/users/banwang3/celllines/analysis/DESeq2_norm/human/',celltype,'_aligned2human_qvalue_DESeq2.txt'),sep='\t')
+    deseq2m = read.csv(paste0('/scratch/users/banwang3/celllines/analysis/DESeq2_norm/merged/',celltype,'_merged_qvalue_DESeq2.txt'),sep='\t')
+    deseq2$flag = deseq2$gene %in% deseq2m$gene
+    deseq2=deseq2[,c('gene','log2FoldChange','qval_mine','flag')]
+    colnames(deseq2)=c('gene',paste0(celltype,'_lfc'),paste0(celltype,'_FDR'),paste0(celltype,'_flag'))
+    
+    if (length(final)==0){
+        final=deseq2
+    }else{
+        final = merge(final,deseq2)
+    }
+}
+tmp = merge(final,TGF)
+dim(tmp)
+tmp2 = tmp[order(tmp[,'MN_lfc']),]
+data=c()
+for (i in seq(1,nrow(tmp2))){
+    for (celltype in c('CM','MN','PP','HP','SKM','RPE')){
+        coln=paste0(celltype,'_lfc')
+        flagn=paste0(celltype,'_flag')
+        if(tmp2[i,flagn]){
+            labelext = '*'
+        }else{labelext=''}    
+        
+        data = rbind(data,c(tmp2[i,'gene'],tmp2[i,coln],celltype,labelext))
+    }
+}
+data = data.frame(data,stringsAsFactors =F)
+colnames(data)=c('gene','LFC','celltype','DE')
+data$LFC=as.numeric(data$LFC)
+data$celltype=factor(data$celltype,levels=c('MN','CM','HP','PP','SKM','RPE'))
+output_path='/scratch/users/banwang3/celllines/scripts/RScripts/Rplots11/'
+pdf(paste0(output_path,'MN_set_LFC.pdf'),useDingbats=FALSE,width=4,height=3)
+print(max(data$LFC))
+print(min(data$LFC))
+ggplot(data, aes(factor(celltype), gene)) + 
+  geom_tile(aes(fill=LFC),color='black',height=1,width=1)+
+  geom_text(aes(label=DE))+
+  scale_fill_gradientn(colours = c("dodgerblue4", "white", "indianred3"),limits=c(-2.1,2.1))+
+  xlab('')+
+  ylab('')+
+  theme_bw()
+dev.off()
